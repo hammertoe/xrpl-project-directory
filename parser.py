4879c494945275a015e61ebd5c4454d033ca1d38
Matt Hamilton <mhamilton@ripple.com>
2022-02-22 16:24:56 -0400

    return a non-zero error code on failure, so that github actions know we failed

--- parser.py
+++ parser.py
@@ -1,11 +1,15 @@
 import datetime
 import os
+import sys
 import toml
 
 data_path = "data"
 mandatory_fields = ["title", "url", "modified_date", "category"]
 
 def parse_files(data_path):
+    failed_files = []
+    successful_files = []
+    
     for root, dirs, files in os.walk(data_path):
         for file in files:
             if(file.endswith(".toml")):
@@ -15,6 +19,7 @@ def parse_files(data_path):
                     data = toml.load(path)
                 except toml.decoder.TomlDecodeError as e:
                     print(f"ERROR: {path} failed to decode: {e}")
+                    failed_files.append(path)
                     continue
                     
                 # Check mandatory fields
@@ -24,6 +29,7 @@ def parse_files(data_path):
                         missing_fields.append(field)
                 if missing_fields:
                     print(f"ERROR: {path} is missing fields {', '.join(missing_fields)}")
+                    failed_files.append(path)
                     # skip rest of processing for this file
                     continue
 
@@ -32,9 +38,22 @@ def parse_files(data_path):
                     last_modified = datetime.datetime.fromisoformat(data['modified_date'])
                 except ValueError:
                     print(f"ERROR: {path} invalid modified_date field")
+                    failed_files.append(path)
                     continue
+
+                # If we reach here our file has been validated successfully
+                successful_files.append(path)
+
+    return successful_files, failed_files
 
 
 if __name__ == "__main__":
-    parse_files(data_path)
+    successful_files, failed_files = parse_files(data_path)
 
+    print()
+    print(f"Sucessfully parsed {len(successful_files)} files.")
+    print(f"Failed files: {len(failed_files)}.")
+
+    # Return non-zero error code to signal to other processes
+    if failed_files:
+        sys.exit(1)
