2f930eb45a2eb9ac7ebdcafe652d6ffc3b60e745
Matt Hamilton <mhamilton@ripple.com>
2022-02-22 16:04:21 -0400

    Validate that the mandatory fields are present in each file

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import toml
 import os
 
 data_path = "data"
-
+mandatory_fields = ["title", "url", "modified_date", "category"]
 
 def parse_files(data_path):
     for root, dirs, files in os.walk(data_path):
@@ -14,7 +14,16 @@ def parse_files(data_path):
                     data = toml.load(path)
                 except toml.decoder.TomlDecodeError as e:
                     print("Failed to decode: ", path, e)
-            
+                    
+                # Check mandatory fields
+                missing_fields = []
+                for field in mandatory_fields:
+                    if field not in data:
+                        missing_fields.append(field)
+                if missing_fields:
+                    print(f"ERROR: {path} is missing fields {', '.join(missing_fields)}")
+                    # skip rest of processing for this file
+                    continue
 
 if __name__ == "__main__":
     parse_files(data_path)
